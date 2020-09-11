ae47a0e818e0f085a9f8ff97a521ccaddffda43b
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-09-11 22:59:11 +0200

    Refactor

--- com/example/avaj_launcher/Simulator.java
+++ com/example/avaj_launcher/Simulator.java
@@ -11,18 +11,24 @@ public class Simulator {
 	public static PrintWriter writer;
 
 	public static void main(String [] args) {
-		WeatherTower weatherTower = new WeatherTower();
-		AircraftFactory aircraftFactory = new AircraftFactory();
-		int numSimulations = 0;
-
 		try {
-			writer = new PrintWriter("simulation.txt");
-		} catch (FileNotFoundException fnfe) { 
-			System.out.println(fnfe.getMessage());
-			return;
-		}
+			WeatherTower weatherTower = new WeatherTower();
+			AircraftFactory aircraftFactory = new AircraftFactory();
+			int numSimulations = 0;
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+			// Check argument
+			if (args.length == 0) {
+				System.out.println("Missing argument.");
+				System.out.println("Usage: java com.example.avaj_launcher.Simulator scenario.txt");
+				System.out.println("");
+				return;
+			}
+
+			// Open ouput file
+			writer = new PrintWriter("simulation.txt");
+
+			// Parse input file
+			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
 			String line = reader.readLine();
 			if (line == null) {
 				System.out.println("Error: file is empty");
@@ -48,14 +54,21 @@ public class Simulator {
 				weatherTower.register(x);
 				x.registerTower(weatherTower);
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
 
-		for (int i=0; i<numSimulations; i++) {
-			weatherTower.changeWeather();
+			// Start simulation
+			for (int i=0; i<numSimulations; i++) {
+				weatherTower.changeWeather();
+			}
+			writer.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: cannot open file");
+		} catch (IOException e) {
+			System.out.println("Error: IO exception");
+		} catch (NumberFormatException e) {
+			System.out.println("Error: cannot parse integer");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		writer.close();
 	}
 }
